db4d2b4177f8fd43e6fce523134dfc110efdb7b9
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-08-09 15:40:40 +0500

    feat: add a ship class

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-console.log('Running');
 import './styles.css';
---
+++ src/ship.js
@@ -0,0 +1,16 @@
+class ship {
+  constructor(length) {
+    this.length = length;
+    this.hitTimes = 0;
+    this.sunk = false;
+  }
+  hit() {
+    this.hitTimes += 1;
+  }
+  isSunk() {
+    if (this.hitTimes >= this.length) {
+      this.sunk = true;
+    }
+    return this.sunk;
+  }
+}
